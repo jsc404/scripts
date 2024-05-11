eb844850ed15b0fc8a9cfa48dceadd6e4da5950b
u8717 <167609260+u8717@users.noreply.github.com>
2024-05-11 16:40:26 +0200

    Create get-instance-ip.sh

---
+++ get-instance-ip.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Check if a VM name was provided
+if [[ -z $1 ]]; then
+  echo "Usage: ./get-instance-ip.sh <VM_NAME>"
+  exit 1
+fi
+
+VM_NAME="$1"
+MAX_RETRIES=10
+DELAY=5 
+
+for ((i=1; i<=MAX_RETRIES; i++)); do
+  IP_ADDRESS=$(virsh net-dhcp-leases default | grep ${VM_NAME} | awk '{print $5}' | cut -d '/' -f 1)
+  if [[ -z $IP_ADDRESS ]]; then
+    echo "Waiting for IP assignment (attempt $i/$MAX_RETRIES)..."
+    sleep $DELAY
+  else
+    echo "${IP_ADDRESS}"  # Output just the IP itself
+    exit 0  # Success!
+  fi
+done
+
+echo "Error: Could not get IP for ${VM_NAME}"
+exit 1 
